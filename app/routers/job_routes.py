4cbdd986863e377a630eaf0857cc411e6f24f420
jgwentworth92 <alphabetboi92@protonmail.com>
2024-06-28 14:08:56 -0400

    minor formatting

--- app/routers/job_routes.py
+++ app/routers/job_routes.py
@@ -53,8 +53,6 @@ async def create_document(document: DocumentCreate, db: AsyncSession = Depends(g
     created_document = await JobService.create_document(db, document.dict())
     return DocumentResponse.model_construct(**created_document.__dict__)
 
-from typing import List
-
 @router.post("/documents/batch/", response_model=List[DocumentResponse], status_code=status.HTTP_201_CREATED,
              tags=["Document Management"])
 async def create_multiple_documents(documents: List[DocumentCreate], db: AsyncSession = Depends(get_db)):
